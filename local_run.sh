db1be7f32fa7b2a29386e6629eabd5b9583815b7
Christos Vontas <christos@redsift.io>
2017-09-14 15:51:44 +0100

    updated the local script

--- local_run.sh
+++ local_run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# git clone https://github.com/redsift/go-sandbox-rpc
+# git clone https://github.com/redsift/go-sandbox-rpc root/usr/lib/redsift/workspace/src/github.com/redsift/go-sandbox-rpc
 
 DEV_LOC=/Users/chrisvon/Documents/Developer/redsift
 docker run \
--v ${DEV_LOC}/sifts/hello-go-sift:/run/sandbox/sift \
+-v ${DEV_LOC}/sandbox-go/testFixtures/sift:/run/sandbox/sift \
 -ti gotest
 
 # -v ${DEV_LOC}/sandbox-go:/usr/lib/redsift/sandbox/src/sandbox-go \
