81ed359fd6611e7018bd0c51e677c1abceffefc1
Amnon <amnon.cohen@redsift.io>
2020-11-10 15:29:30 +0000

    try in /tmp/

--- cmd/install/install.go
+++ cmd/install/install.go
@@ -15,7 +15,7 @@ const SANDBOX_PATH = "/run/sandbox/sift/"
 const PROJECT_LOCATION = SANDBOX_PATH
 
 //const SIFT_GO_LOCATION = PROJECT_LOCATION + "/sandbox/sift.go"
-const SIFT_GO_LOCATION = PROJECT_LOCATION + "/tmp/sift.go"
+const SIFT_GO_LOCATION = "/tmp/sift.go"
 
 const siftTemp = `package sandbox
 
