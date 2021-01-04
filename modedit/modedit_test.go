9d14fa919e26b297dc9805ab062bc25e73870739
Amnon <amnon.cohen@redsift.io>
2021-01-04 17:18:35 +0000

    add function to update module files copying replace directives from one to the other

---
+++ modedit/modedit_test.go
@@ -0,0 +1,39 @@
+package modedit
+
+import (
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+const expectedOutput=`
+module github.com/redsift/sandbox-go
+
+go 1.15
+
+require (
+	github.com/redsift/go-mangosock v0.2.0
+	github.com/redsift/go-sandbox-rpc v0.1.0
+	golang.org/x/mod v0.4.0
+)
+
+replace server => /run/sandbox/sift/server
+
+replace github.com/blevesearch/bleve v1.0.14 => github.com/redsift/bleve v0.5.1-0.20201222154652-c8a1b8b0f852
+
+replace github.com/tecbot/gorocksdb => github.com/redsift/gorocksdb v0.0.0-20180109115255-d1d69065a9b9
+`
+
+func TestCopyReplace(t *testing.T) {
+	out := filepath.Join(t.TempDir(), "new.mod")
+	err := CopyReplace("testdata/insights.mod", "testdata/sandbox.mod", out)
+	require.NoError(t, err)
+
+	res, err := ioutil.ReadFile(out)
+	require.NoError(t, err)
+	assert.Equal(t, strings.TrimSpace(expectedOutput), strings.TrimSpace(string(res)))
+}
---
+++ modedit/testdata/insights.mod
@@ -0,0 +1,20 @@
+module server
+
+go 1.15
+
+require (
+	github.com/blevesearch/bleve v1.0.14
+	github.com/blevesearch/blevex v1.0.0
+	github.com/cenkalti/backoff/v4 v4.1.0
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/redsift/go-sandbox-rpc v0.0.0-20200511193908-a6e5d4529bf7
+	github.com/redsift/go-stats v0.2.0
+	github.com/stretchr/testify v1.6.1
+	golang.org/x/sys v0.0.0-20201223074533-0d417f636930 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
+)
+
+replace (
+	github.com/blevesearch/bleve v1.0.14 => github.com/redsift/bleve v0.5.1-0.20201222154652-c8a1b8b0f852
+	github.com/tecbot/gorocksdb => github.com/redsift/gorocksdb v0.0.0-20180109115255-d1d69065a9b9
+)
