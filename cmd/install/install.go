318b24020ed74d31d20e2505a248b88ffce185b9
Deepak Prabhakara <deepak.prabhakara@gmail.com>
2021-01-04 21:28:58 +0000

    moved modedit inside sandbox

--- cmd/install/install.go
+++ cmd/install/install.go
@@ -8,8 +8,8 @@ import (
 	"strings"
 	"text/template"
 
-	"github.com/redsift/sandbox-go/modedit"
 	"github.com/redsift/sandbox-go/sandbox"
+	"github.com/redsift/sandbox-go/sandbox/modedit"
 )
 
 const PROJECT_LOCATION = "/build/"
